a1f1909359a7466785bb6660bec1d196be005d02
Aljaz Vrhovnik <aljazvrhovnik@Aljazs-MacBook-Pro.local>
2020-08-10 10:18:43 +0200

    Dodan enkoder func in servo

--- cajnik_project.ino
+++ cajnik_project.ino
@@ -1,71 +1,75 @@
 #include <SeeedOLED.h>//grove lib-glej example!mogoce rabs se i2c inicializacijo in njeno kniznco
 #include <Servo.h>
 #include <EEPROM.h>
-
-//rabs interrupt za timer in mogoce enkoder(pin int.) pazi na fronto proženja nastavitev
-
+#include <Wire.h>//i2c lib
+//encoder***************
+#define outputA 6
+#define outputB 7
+int counter = 0;
+int aState;
+int aLastState;
+//encoder end*********
+//servo+++++++++++++
+#define servo_pin 9
 Servo servo;
 int position_of_servo = 0;
 int *ptr_of_servo_pos = &position_of_servo;
-int timer_naslov_eeprom = 0; //naslov
+//servo end++++++++++
+int timer_addr_eeprom = 0; //naslov
 void setup() {
+  //i2c in oled
+  Wire.begin();
+  SeeedOled.init();  //initialze SEEED OLED display
+  SeeedOled.clearDisplay();          //clear the screen and set start position to top left corner
+  SeeedOled.setNormalDisplay();      //Set display to normal mode (i.e non-inverse mode)
+  SeeedOled.setPageMode();           //Set addressing mode to Page Mode
+  SeeedOled.setTextXY(0, 0);         //Set the cursor to Xth Page, Yth Column
+  SeeedOled.putString("Cajnik!"); //Print the String
+  //encoder
+  pinMode (outputA, INPUT);
+  pinMode (outputB, INPUT);
+  aLastState = digitalRead(outputA);
+  //uart
   Serial.begin(9600);
-  servo.attach(9);
+  //servo pin:
+  servo.attach(servo_pin);
 }
 
 //da se ne ukvarjas s prekinitvami uporab nizko vrednost za sestevalnika za eno sekundo... primer delay na 10ms(1000/10=100 proženj)
 
 void loop() {
-  if (Serial.available()) {
-    position_of_servo = Serial.parseInt();
-    servo.write(position_of_servo);
-    Serial.write("\nPozicija sevota je:");
-    Serial.print(position_of_servo);
-    delay(100);
-  }
-
-
+  read_encoder_state();
 }
 
 
-void servo_trenutna_pozicija(){
-    //uporabljena funkcija pri odštevanju časa in pa nastavitev_visine
-  }
+void servo_position_set(int position) {
+  //uporabljena funkcija pri odštevanju časa in pa nastavitev_visine
+  servo.write(position);
+}
 
-void nastavitev_visine(){
+void servo_hight_adjustment() {
   //enako kot spodaj
-  }
+}
 
 void set_timer() {
- //če je stanje drugačno kot prej preglej ali se timer povečuje ali manjša
- //se prav da je a signal pred bjom-se veča in obratno se man
+  //če je stanje drugačno kot prej preglej ali se timer povečuje ali manjša
+  //se prav da je a signal pred bjom-se veča in obratno se man
 }
-/*
- * primer za nastavitev časa z enkoderjom(https://howtomechatronics.com/tutorials/arduino/rotary-encoder-works-use-arduino/):
-  aState = digitalRead(outputA);
-  
-  if (aState != aLastState){     
-     if (digitalRead(outputB) != aState) { 
-       counter ++;
-       angle ++;
-       rotateCW();  
-     }
-     else {
-       counter--;
-       angle --;
-       rotateCCW(); 
-     }
-     if (counter >=30 ) {
-      counter =0;
-     }
-     
-     lcd.clear();
-     lcd.print("Position: ");
-     lcd.print(int(angle*(-1.8)));
-     lcd.print("deg"); 
-     lcd.setCursor(0,0);
-     
-   }
-  aLastState = aState;
+
+void read_encoder_state() {
+  aState = digitalRead(outputA); // Reads the "current" state of the outputA
+  // If the previous and the current state of the outputA are different, that means a Pulse has occured
+  if (aState != aLastState) {
+    // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
+    if (digitalRead(outputB) != aState) {
+      if (counter < 254)counter ++;
+    } else {
+      if (counter > 1)counter --;
+    }
+    Serial.print("Position: ");
+    Serial.println(counter);
+    servo_position_set(counter);
+  }
+  aLastState = aState; // Updates the previous state of the outputA with the current state
+
 }
-*/
