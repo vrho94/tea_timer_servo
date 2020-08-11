2e345b028c6c8c12e5156770ea2d02e0eb6c80f7
Aljaz Vrhovnik <aljazvrhovnik@Aljazs-MacBook-Pro.local>
2020-08-11 14:03:38 +0200

    Dela skupi enkoder, oled,servo... zajbava gumb na enkoderju?

--- cajnik_project.ino
+++ cajnik_project.ino
@@ -1,7 +1,24 @@
-#include <SeeedOLED.h>//grove lib-glej example!mogoce rabs se i2c inicializacijo in njeno kniznco
+#include <ssd1306_uart.h>
+#include <ssd1306_8bit.h>
+#include <nano_gfx_types.h>
+#include <nano_engine.h>
+#include <ssd1306_16bit.h>
+#include <nano_gfx.h>
+#include <ssd1306.h>
+#include <font6x8.h>
+#include <ssd1306_console.h>
+#include <ssd1306_generic.h>
+#include <sprite_pool.h>
+#include <ssd1306_1bit.h>
+#include <ssd1306_fonts.h>
+
 #include <Servo.h>
 #include <EEPROM.h>
-#include <Wire.h>//i2c lib
+
+/*
+  pini:
+  enkoder: modra gnd,zelena 3.3v,rumena DT, oranzna clk
+*/
 //encoder***************
 #define outputA 6
 #define outputB 7
@@ -10,33 +27,34 @@ int aState;
 int aLastState;
 //encoder end*********
 //servo+++++++++++++
-#define servo_pin 9
+#define servo_pin 8
 Servo servo;
 int position_of_servo = 0;
 int *ptr_of_servo_pos = &position_of_servo;
 //servo end++++++++++
+//gumb
+const int gumb = 9;     // the number of the pushbutton pin
 int timer_addr_eeprom = 0; //naslov
 void setup() {
-  //i2c in oled
-  Wire.begin();
-  SeeedOled.init();  //initialze SEEED OLED display
-  SeeedOled.clearDisplay();          //clear the screen and set start position to top left corner
-  SeeedOled.setNormalDisplay();      //Set display to normal mode (i.e non-inverse mode)
-  SeeedOled.setPageMode();           //Set addressing mode to Page Mode
-  SeeedOled.setTextXY(0, 0);         //Set the cursor to Xth Page, Yth Column
-  SeeedOled.putString("Cajnik!"); //Print the String
+  //oled
+  ssd1306_128x64_i2c_init();
+  ssd1306_fillScreen(0x00);
+  ssd1306_setFixedFont(ssd1306xled_font6x8);
+  ssd1306_printFixed (0,  8, "Cajnik", STYLE_NORMAL);
   //encoder
   pinMode (outputA, INPUT);
   pinMode (outputB, INPUT);
   aLastState = digitalRead(outputA);
   //uart
-  Serial.begin(9600);
+  Serial.begin(9600);//ne rabs!!!samo za debugging
   //servo pin:
   servo.attach(servo_pin);
+  //gumb
+  //pinMode(gumb, INPUT);
 }
 
 //da se ne ukvarjas s prekinitvami uporab nizko vrednost za sestevalnika za eno sekundo... primer delay na 10ms(1000/10=100 proženj)
-
+int gumb_stanje;
 void loop() {
   read_encoder_state();
 }
@@ -48,7 +66,12 @@ void servo_position_set(int position) {
 }
 
 void servo_hight_adjustment() {
-  //enako kot spodaj
+  if(digitalRead(gumb)==HIGH){
+  ssd1306_printFixedN (0,  8, "Cajnik naklon vodila:", STYLE_BOLD,0.2);
+  while(digitalRead(gumb)==HIGH);
+  while(digitalRead(gumb)==LOW)read_encoder_state();
+  ssd1306_printFixedN (0,  8, "Konc:", STYLE_BOLD,0.2);
+  }
 }
 
 void set_timer() {
@@ -62,14 +85,19 @@ void read_encoder_state() {
   if (aState != aLastState) {
     // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
     if (digitalRead(outputB) != aState) {
-      if (counter < 254)counter ++;
+      if(counter<0)counter=0;
+      else counter ++;
     } else {
-      if (counter > 1)counter --;
+      if(counter>180)counter=180;
+      else counter --;
     }
     Serial.print("Position: ");
     Serial.println(counter);
+    //ssd1306_drawProgressBar( counter/1.5 );
     servo_position_set(counter);
   }
   aLastState = aState; // Updates the previous state of the outputA with the current state
-
+}
+void write_number_to_string_on_oled(int x, int y,int num, char string[]) {
+  ssd1306_printFixed (x,  y, itoa(num, string, 4), STYLE_NORMAL);
 }
