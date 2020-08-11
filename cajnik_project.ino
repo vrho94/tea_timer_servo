8329186f6e5beac0b77855197e28ba85de903587
Aljaz Vrhovnik <aljazvrhovnik@Aljazs-MacBook-Pro.local>
2020-08-11 15:06:55 +0200

    Nastavljanje visine in timerja ne dela prov, treba bo popravt funkcijo za baranje enkoderja

--- cajnik_project.ino
+++ cajnik_project.ino
@@ -22,7 +22,7 @@
 //encoder***************
 #define outputA 6
 #define outputB 7
-int counter = 0;
+//int counter = 0;
 int aState;
 int aLastState;
 //encoder end*********
@@ -35,6 +35,9 @@ int *ptr_of_servo_pos = &position_of_servo;
 //gumb
 const int gumb = 9;     // the number of the pushbutton pin
 int timer_addr_eeprom = 0; //naslov
+int max_pos = 0;
+int min_pos = 0;
+int minute = 0;
 void setup() {
   //oled
   ssd1306_128x64_i2c_init();
@@ -56,52 +59,91 @@ void setup() {
 //da se ne ukvarjas s prekinitvami uporab nizko vrednost za sestevalnika za eno sekundo... primer delay na 10ms(1000/10=100 proženj)
 int gumb_stanje;
 void loop() {
-  if(digitalRead(gumb)==LOW){
-    ssd1306_printFixedN (0,  8, "gumb dela!", STYLE_BOLD,0.2);
-    delay(1500);
-    }
-  read_encoder_state();
+  servo_hight_adjustment();
 }
 
-
-void servo_position_set(int position) {
-  //uporabljena funkcija pri odštevanju časa in pa nastavitev_visine
-  servo.write(position);
-}
 
 void servo_hight_adjustment() {
-  if(digitalRead(gumb)==HIGH){
-  ssd1306_printFixedN (0,  8, "Cajnik naklon vodila:", STYLE_BOLD,0.2);
-  while(digitalRead(gumb)==HIGH);
-  while(digitalRead(gumb)==LOW)read_encoder_state();
-  ssd1306_printFixedN (0,  8, "Konc:", STYLE_BOLD,0.2);
+  if (digitalRead(gumb) == LOW) {
+    char num_str_buf[4];
+    ssd1306_fillScreen(0x00);
+    ssd1306_printFixedN (0,  8, "Nastavi visino(MAX):", STYLE_BOLD, 0.2);
+    while (digitalRead(gumb) == LOW);
+    delay(200);
+    while (digitalRead(gumb) == HIGH) {
+      max_pos = read_encoder_state(max_pos);
+      if(max_pos<0)max_pos=0;
+      if(max_pos>180)max_pos=180;
+    }
+    ssd1306_fillScreen(0x00);
+    ssd1306_printFixedN (0,  8, "Nastavi visino(MIN):", STYLE_BOLD, 0.2);
+    while (digitalRead(gumb) == LOW);
+    delay(200);
+    while (digitalRead(gumb) == HIGH) {
+      min_pos = read_encoder_state(min_pos);
+      if(min_pos<0)min_pos=0;
+      if(min_pos>180)min_pos=180;
+    }
+    if (max_pos > 180)max_pos = 180;
+    if (max_pos < 0)min_pos = 0;
+    if (min_pos > 180)max_pos = 180;
+    if (min_pos < 0)min_pos = 0;
+    ssd1306_fillScreen(0x00);
+    ssd1306_printFixedN (0,  8, "Izhod!", STYLE_BOLD, 0.2);
+    ssd1306_printFixed (0,  16, "MAX:", STYLE_NORMAL);
+    ssd1306_printFixed (0,  32, "MIN:", STYLE_NORMAL);
+    ssd1306_printFixed (40,  16, itoa(max_pos, num_str_buf, 3), STYLE_NORMAL);
+    ssd1306_printFixed (40,  32, itoa(min_pos, num_str_buf, 3), STYLE_NORMAL);
+
+    while (digitalRead(gumb) == LOW);
+    delay(200);
+
   }
 }
 
 void set_timer() {
+  if (digitalRead(gumb) == LOW) {
+    ssd1306_fillScreen(0x00);
+    ssd1306_printFixedN (0,  8, "Nastavi cas(min):", STYLE_BOLD, 0.2);
+    while (digitalRead(gumb) == LOW);
+    delay(200);
+    while (digitalRead(gumb) == HIGH) {
+      minute = read_encoder_state(minute);
+     // ssd1306_printFixed (40,  16, itoa(minute, num_str_buf, 4), STYLE_NORMAL);
+    }
+    ssd1306_printFixedN (0,  8, "Izhod!", STYLE_BOLD, 0.2);
+    ssd1306_printFixed (0,  16, "MAX:", STYLE_NORMAL);
+    ssd1306_printFixed (0,  32, "MIN:", STYLE_NORMAL);
+//    ssd1306_printFixed (40,  16, itoa(max_pos, num_str_buf, 4), STYLE_NORMAL);
+ //   ssd1306_printFixed (40,  32, itoa(min_pos, num_str_buf, 4), STYLE_NORMAL);
+
+    while (digitalRead(gumb) == LOW);
+    delay(200);
+  }
   //če je stanje drugačno kot prej preglej ali se timer povečuje ali manjša
   //se prav da je a signal pred bjom-se veča in obratno se man
 }
 
-void read_encoder_state() {
+int read_encoder_state(int counter) {
   aState = digitalRead(outputA); // Reads the "current" state of the outputA
   // If the previous and the current state of the outputA are different, that means a Pulse has occured
   if (aState != aLastState) {
     // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
     if (digitalRead(outputB) != aState) {
-      if(counter<0)counter=0;
+      if (counter < 0)counter = 0;
       else counter ++;
     } else {
-      if(counter>180)counter=180;
+      if (counter > 180)counter = 180;
       else counter --;
     }
     Serial.print("Position: ");
     Serial.println(counter);
     //ssd1306_drawProgressBar( counter/1.5 );
-    servo_position_set(counter);
+    servo.write(counter);
   }
   aLastState = aState; // Updates the previous state of the outputA with the current state
+  return counter;
 }
-void write_number_to_string_on_oled(int x, int y,int num, char string[]) {
+void write_number_to_string_on_oled(int x, int y, int num, char string[]) {
   ssd1306_printFixed (x,  y, itoa(num, string, 4), STYLE_NORMAL);
 }
